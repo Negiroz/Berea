7558a29c0b6af35766f7566b8dc5062033c9949b
Negiroz <ramirogonzg@gmail.com>
2019-12-27 15:15:11 -0400

    creacion de dos tablas distrito y evento

---
+++ database/migrations/2019_12_27_191344_crear_tabla_evento.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaEvento extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evento', function (Blueprint $table) {
+            $table->bigIncrements('idEvento');
+            $table->string('nombreEvento',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evento');
+    }
+}
